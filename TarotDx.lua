050d9df2294909b0f74d7d035f1313f5fb699ded
Dimserene <raul0520@gmail.com>
2024-07-03 18:18:04 +0800

    fix

--- TarotDx.lua
+++ TarotDx.lua
@@ -3012,7 +3012,7 @@ local function overrides()
                 G.STATE = G.STATES.BUFFOON_PACK
                 G.GAME.pack_size = self.ability.extra
             elseif self.ability.name:find('Alchemy') then
-                G.STATES.STANDARD_PACK
+                G.STATE = G.STATES.STANDARD_PACK
                 G.GAME.pack_size = self.ability.extra
             end
 
